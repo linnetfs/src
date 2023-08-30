1f7de9b749218ef0c5fc63d56b8cbf5e36d7e8ab
Jeremías <jrmsdev@gmail.com>
2023-08-30 16:13:55 -0300

    lnfs_memory: getattr

--- include/lnfs_memory.hpp
+++ include/lnfs_memory.hpp
@@ -8,6 +8,8 @@
 
 #include "lnfs_fuse.hpp"
 
+#include <time.h>
+
 #include <string>
 
 using namespace std;
@@ -17,13 +19,31 @@ using namespace std;
 
 class File {
 	public:
+		int mode;
+		int nlink;
+		int uid;
+		int gid;
+		int size;
+		int blocks;
+		time_t atime;
+		time_t mtime;
+		time_t ctime;
+		bool not_found;
+
+		int getattr(struct stat* stbuf);
+
 		void dirent();
 		bool is_dir();
+		bool is_path(string path);
+		string name();
+
+		File();
 		File(string p);
 
 	private:
 		string path;
 		bool dir;
+		void init(string p);
 };
 
 File lnfs_dir(string path);
@@ -33,11 +53,13 @@ File lnfs_dir(string path);
 
 class LNFS {
 	public:
-		LNFS();
+		File get(string p);
+		LNFS(string n);
 
 	private:
+		string name;
 		int next;
-		File* files[1000];
+		File files[1000];
 };
 
 /******************************************************************************/
@@ -46,3 +68,5 @@ class LNFS {
 const struct fuse_operations* lnfs_operations();
 
 void* lnfs_init(struct fuse_conn_info* conn, struct fuse_config* cfg);
+
+int lnfs_getattr(const char* path, struct stat* stbuf, struct fuse_file_info* fi);
--- lib/lnfs_memory.cpp
+++ lib/lnfs_memory.cpp
@@ -7,9 +7,24 @@
 /******************************************************************************/
 // File
 
+int File::getattr(struct stat* stbuf)
+{
+	stbuf->st_mode = mode;
+	stbuf->st_nlink = nlink;
+	stbuf->st_uid = uid;
+	stbuf->st_gid = gid;
+	stbuf->st_size = size;
+	stbuf->st_blocks = blocks;
+	stbuf->st_atime = atime;
+	stbuf->st_mtime = mtime;
+	stbuf->st_ctime = ctime;
+	return 0;
+}
+
 void File::dirent()
 {
 	dir = true;
+	mode = 0755;
 }
 
 bool File::is_dir()
@@ -17,10 +32,38 @@ bool File::is_dir()
 	return dir;
 }
 
-File::File(string p)
+bool File::is_path(string p)
+{
+	return path == p;
+}
+
+string File::name()
+{
+	return path;
+}
+
+void File::init(string p)
 {
 	path = p;
 	dir  = false;
+	mode = 0644;
+	nlink = 1;
+	uid = 0;
+	gid = 0;
+	size = 0;
+	blocks = 0;
+	atime = mtime = ctime = time(NULL);
+	not_found = false;
+}
+
+File::File()
+{
+	init("/");
+}
+
+File::File(string p)
+{
+	init(p);
 }
 
 File lnfs_dir(string path)
@@ -33,14 +76,31 @@ File lnfs_dir(string path)
 /******************************************************************************/
 // LNFS
 
-LNFS::LNFS()
+File LNFS::get(string p)
 {
-	File root = lnfs_dir("/");
-	files[0] = &root;
+	lnfs_debug("lnfs get {}", p);
+	for (File f : files)
+	{
+		lnfs_debug("f.path {}", f.name());
+		if (f.is_path(p))
+			return f;
+	}
+	lnfs_debug("{}: lnfs file or directory not found", p);
+	File f(p);
+	f.not_found = true;
+	return f;
+}
+
+LNFS::LNFS(string n)
+{
+	name = n;
+	File root("/");
+	root.dirent();
+	files[0] = root;
 	next = 1;
 }
 
-static const LNFS fs;
+static LNFS fs("memory");
 
 /******************************************************************************/
 // init
@@ -57,10 +117,27 @@ void* lnfs_init(struct fuse_conn_info* conn, struct fuse_config* cfg)
 }
 
 /******************************************************************************/
+// getattr
+
+int lnfs_getattr(const char* path, struct stat* stbuf, struct fuse_file_info* fi)
+{
+	lnfs_debug("memory getattr {}", path);
+	File f = fs.get(path);
+	if (f.not_found)
+	{
+		int rc = -ENOENT;
+		lnfs_error("memory getattr {} {}", path, rc);
+		return rc;
+	}
+	return f.getattr(stbuf);
+}
+
+/******************************************************************************/
 // operations
 
 static const struct fuse_operations ops = {
-	.init = lnfs_init,
+	.getattr = lnfs_getattr,
+	.init    = lnfs_init,
 };
 
 const struct fuse_operations* lnfs_operations()
